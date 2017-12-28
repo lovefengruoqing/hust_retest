d1c2a985adc0e79bd32a38f015eb7cb6a19c47b4
fengruoqing <1031281783@qq.com>
2017-12-29 00:22:44 +0800

    work on one problem

---
+++ 2017.01.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+void print(char ch);    //打印字符函数
+int main(){
+    char ch;
+    int n=0;
+    do{
+        if(n==0){
+            printf("please enter char, rang are A-Z or a-z: ");
+        }else{
+            printf("Please enter the right char: ");
+        }
+        scanf(" %c",&ch);    //读出输入的字符,注意加个空格，不然会读入缓冲区的内容
+        n++;
+    }while(!((ch<='Z'&&ch>='A')||(ch<='z'&&ch>='a')));
+
+
+    int i,j;
+    int len = ch<'a'?ch-'A':ch-'a';     //判断需要打印的字符个数
+    printf("\nThe console follows: \n");
+    for(i=0;i<=len;i++){
+        j=i;
+        while(j>0){     //打印空格
+            printf(" ");
+            j--;
+        }
+        print(ch);  //调用函数按行打印
+        ch--;
+        printf("\n");
+    }
+
+
+    return 0;
+}
+
+void print(char ch){
+    char temp, flag;
+    flag = ch<'a'?'A':'a';  //确定标志
+    if((ch<='Z'&&ch>='A')||(ch<='z'&&ch>='a')){
+        temp = flag;
+        while(temp<=ch){
+            printf("%c",temp++);
+        }
+        temp = ch-1;
+        while(temp>=flag){
+            printf("%c",temp--);
+        }
+    }
+}
